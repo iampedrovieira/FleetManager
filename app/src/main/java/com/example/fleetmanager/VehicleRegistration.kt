3e45024b9f82db13833d358eda6711ebe1dd67da
marquesantonio2020 <61158023+marquesantonio2020@users.noreply.github.com>
2021-05-20 11:32:09 +0100

    Vehicle registration and description pages

---
+++ app/src/main/java/com/example/fleetmanager/VehicleRegistration.kt
@@ -0,0 +1,11 @@
+package com.example.fleetmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VehicleRegistration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle_registration)
+    }
+}
