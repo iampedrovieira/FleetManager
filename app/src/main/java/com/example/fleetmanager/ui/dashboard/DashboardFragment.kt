e86673decc0faa95a1ef639f11f852633a600b66
marquesantonio2020 <61158023+marquesantonio2020@users.noreply.github.com>
2021-05-24 10:36:04 +0100

    Getting polyline from google maps api from origin to destination (with LATLNG)

--- app/src/main/java/com/example/fleetmanager/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/fleetmanager/ui/dashboard/DashboardFragment.kt
@@ -1,15 +1,31 @@
 package com.example.fleetmanager.ui.dashboard
 
 import android.app.ActionBar
+import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.fleetmanager.MainActivity
 import com.example.fleetmanager.R
+import com.example.fleetmanager.dto.GoogleMapDTO
+import com.example.fleetmanager.dto.PolyLine
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-class DashboardFragment : Fragment() {
+private lateinit var gMap : GoogleMap
+private lateinit var teste:Button
+class DashboardFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var toolbar : androidx.appcompat.widget.Toolbar
 
@@ -23,6 +39,123 @@ class DashboardFragment : Fragment() {
         toolbar = root.findViewById(R.id.toolbar)
         toolbar.title = getString(R.string.title_dashboard)
 
+        teste = root.findViewById(R.id.teste)
+        teste.setOnClickListener {
+           val url = getDirectionUrl(41.15822462366187, -8.62920595465106, 41.354887806044395, -8.747645333285346)
+            GetDirection(url).execute()
+        }
+
+        val mapFragment =  childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         return root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+       gMap = googleMap
+
+        val currentLatLng = LatLng(41.15822462366187, -8.62920595465106)
+        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+    }
+
+    private fun getDirectionUrl(origin_lat: Double, origin_long: Double, destination_lat: Double, destination_long: Double) : String{
+        //origin of route
+        val str_origin = "origin=" + origin_lat + "," + origin_long
+
+        //origin of route
+        val str_destination = "destination=" + destination_lat + "," + destination_long
+
+        val mode = "mode=driving"
+
+        val parameters = "$str_origin&$str_destination&$mode"
+
+        //Output format
+        val output = "json"
+
+        return "https://maps.googleapis.com/maps/api/directions/$output?$parameters&key=AIzaSyAUWK2jFqvY1X2QyHAJLLR9qI1r7hIPIK4"
+    }
+
+    inner class GetDirection(val url: String) : AsyncTask<Void,Void, List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body!!.string()
+            val result = ArrayList<List<LatLng>>()
+            try {
+                val respObj = Gson().fromJson(data, GoogleMapDTO::class.java)
+
+                val path = ArrayList<LatLng>()
+
+                for(i in 0..(respObj.routes[0].legs[0].steps.size-1)){
+                    /**val startLatLng = LatLng(respObj.routes[0].legs[0].steps[i].start_location.lat.toDouble(),
+                        respObj.routes[0].legs[0].steps[i].start_location.lng.toDouble())
+
+                    path.add(startLatLng)
+
+                    val endLatLng = LatLng(respObj.routes[0].legs[0].steps[i].end_location.lat.toDouble(),
+                        respObj.routes[0].legs[0].steps[i].end_location.lng.toDouble())*/
+
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+
+                result.add(path)
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>?) {
+            val lineoption = PolylineOptions()
+
+            for (i in result!!.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.BLUE)
+                lineoption.geodesic(true)
+            }
+
+            gMap.addPolyline(lineoption)
+        }
+
+    }
+
+    public fun decodePolyline(encoded: String): List<LatLng> {
+
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+
+        return poly
     }
 }
